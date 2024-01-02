5bd2d8bdafa35bd6461ec5ea19a40ba849bbe9a2
bigwhoman <hm.keshvari@gmail.com>
2024-01-02 17:49:58 +0330

    completed the gaussian elimination DAG

--- task_generator.py
+++ task_generator.py
@@ -1,5 +1,5 @@
 import sys
-
+from pprint import pprint
 
 class generate_tasks :
     base_task = {
@@ -18,23 +18,39 @@ class generate_tasks :
     
     # Gaussian Elimination DAG generation
     def GE(self, m:int)  -> dict:
-        all_graph_nodes = ( m * m + m - 2)/2
-        all_nodes = []
-        first_node = dict(self.base_task)
-        for i in range(2,m-1 + 2) :
-            child = dict(self.base_task)
-            child["ID"] = i
-            child["Fathers"].append(first_node)
-            first_node["Children"].append(child)
-        for i in range(2, all_graph_nodes + 1) :
-            pass
+        all_graph_nodes = int(( m * m + m - 2)/2)
+        all_nodes = {}
+        for node in range(1,all_graph_nodes + 1) :
+            all_nodes[node] = {
+                "ID" : node,
+                "Fathers" : [],
+                "Children" : [],
+                "Computing_Time" : 1
+            }
+            
+        base_pointer = 1
+        while m > 1 :
+            for i in range(base_pointer + 1, base_pointer + m) :
+                all_nodes[i]["Fathers"].append(base_pointer)
+                all_nodes[base_pointer]["Children"].append(i)
+
+            for i in range(base_pointer + 2, base_pointer + m ) :
+                all_nodes[i + m - 1]["Fathers"].append(i)
+                all_nodes[i]["Children"].append(i + m - 1)           
+            
+            base_pointer += m
+            m -= 1
+        
+        return all_nodes
 
 if __name__ == "__main__" :
-    if len(sys.argv) < 3 :
-        print("Format shoud be as follows : method num_of_tasks")
-        exit(1)
-    generation_method = sys.argv[1]
-    number_of_tasks = int(sys.argv[2])
-    x = generate_tasks()
+    # if len(sys.argv) < 3 :
+    #     print("Format shoud be as follows : method num_of_tasks")
+    #     exit(1)
+    # generation_method = sys.argv[1]
+    # number_of_tasks = int(sys.argv[2])
+    number_of_tasks = 5
+    task_generator = generate_tasks()
+    pprint(task_generator.GE(number_of_tasks))
 
 
